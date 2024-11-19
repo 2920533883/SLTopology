2a987373189ce056eb18b75a51610727e612bada
2920533883 <2920533883@qq.com>
2024-11-19 08:49:39 +0100

    SL_8Car

---
+++ inet4.5/out/clang-release/src/inet/common/Compat.o.d
@@ -0,0 +1,13 @@
+../out/clang-release/src/inet/common/Compat.o: \
+  /home/simu5g/omnetpp-6.0/include/omnetpp.h \
+  /home/simu5g/workplace2/inet4.5/src/inet/common/precompiled.h \
+  /home/simu5g/workplace2/inet4.5/src/inet/common/precompiled_release.h \
+  inet/common/Compat.cc inet/common/Compat.h
+
+/home/simu5g/workplace2/inet4.5/src/inet/common/precompiled.h:
+
+/home/simu5g/workplace2/inet4.5/src/inet/common/precompiled_release.h:
+
+inet/common/Compat.cc:
+
+inet/common/Compat.h:
